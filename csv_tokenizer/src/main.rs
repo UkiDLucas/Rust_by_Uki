fb1795959b862baa78c29f6d6bad76e648cc558d
Uki Lucas <UkiDLucas@gmail.com>
2023-12-13 13:35:24 -0800

    Implemented CSV reader to HashMap

--- csv_tokenizer/src/main.rs
+++ csv_tokenizer/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use csv::Reader;
+
+struct CsvTokenizer {
+    file_path: String,
+}
+
+impl CsvTokenizer {
+    fn new(file_path: &str) -> CsvTokenizer {
+        CsvTokenizer {
+            file_path: file_path.to_string(),
+        }
+    }
+
+    fn tokenize(&self) -> Result<HashMap<usize, Vec<String>>, Box<dyn Error>> {
+        let file = File::open(&self.file_path)?;
+        let mut reader = Reader::from_reader(file);
+        let mut result = HashMap::new();
+        for (i, record) in reader.records().enumerate() {
+            let record = record?;
+            result.insert(i, record.iter().map(|s| s.to_string()).collect());
+        }
+        Ok(result)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let tokenizer = CsvTokenizer::new("data/test_data.csv");
+    let tokenized_data = tokenizer.tokenize()?;
+    for (line, tokens) in tokenized_data {
+        println!("Line {}: {:?}", line, tokens);
+    }
+    Ok(())
 }
